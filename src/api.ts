7d4de41758053c8dd3dc9bc9d4e70a2bc698eb39
Steff Beckers <steff@steffbeckers.eu>
2024-12-06 12:01:08 +0100

    Updates on fetching localizations

--- src/api.ts
+++ src/api.ts
@@ -1,26 +1,45 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { Agent } from "https";
 
-export async function fetchLocalizationData(apiUrl: string, cultureName: string): Promise<any> {
-  const url = `${apiUrl}${
-    !apiUrl.endsWith("/") ? "/" : ""
-  }api/abp/application-localization?cultureName=${cultureName}`;
+const httpsAgent = new Agent({
+  // TODO: Create setting for this?
+  rejectUnauthorized: false,
+});
 
-  const httpsAgent = new Agent({
-    // TODO: Create setting for this?
-    rejectUnauthorized: false,
-  });
+const headers = {
+  "Content-Type": "application/json",
+};
 
-  try {
-    const response = await axios.get(url, {
-      httpsAgent,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+export async function fetchLocalizationData(
+  apiUrl: string,
+  cultureNames: string[]
+): Promise<Record<string, any>> {
+  const localizationData: Record<string, any> = {};
 
-    return response.data;
-  } catch (error) {
-    throw new Error(`Failed to fetch localization data: ${error}`);
+  for (const cultureName of cultureNames) {
+    const url = `${apiUrl}${
+      !apiUrl.endsWith("/") ? "/" : ""
+    }api/abp/application-localization?cultureName=${cultureName}`;
+
+    try {
+      const response = await axios.get(url, {
+        httpsAgent,
+        headers,
+      });
+
+      localizationData[cultureName] = response.data;
+    } catch (error) {
+      handleError("Failed to fetch localization data", error);
+    }
   }
+
+  return localizationData;
+}
+
+function handleError(message: string, error: unknown) {
+  throw new Error(
+    `${message}: ${
+      error instanceof AxiosError ? error.code + (error.message ? ": " + error.message : "") : error
+    }`
+  );
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,35 +1,61 @@
 import * as vscode from "vscode";
 import { fetchLocalizationData } from "./api";
 
-export function activate(context: vscode.ExtensionContext) {
+var localizationsByCulture: Record<string, Record<string, string>> = {};
+
+export async function activate(context: vscode.ExtensionContext) {
   console.log("ABP Localization Helper active");
+
+  await fetchLocalizations();
 
   context.subscriptions.push(
     vscode.commands.registerCommand("abp-localization-helper.fetchLocalizations", async () => {
-      const config = vscode.workspace.getConfiguration("abp-localization-helper");
-      const apiUrl = config.get<string>("apiUrl");
-      const defaultCulture = config.get<string>("defaultCulture") || "en";
-
-      if (!apiUrl) {
-        vscode.window.showErrorMessage("API URL is not configured. Please set it in the settings.");
-        return;
-      }
-
-      try {
-        const localizationData = await fetchLocalizationData(apiUrl, defaultCulture);
-
-        vscode.window.showInformationMessage(
-          `Localization data fetched successfully for culture: ${defaultCulture}`
-        );
-
-        console.log(localizationData); // Log or use the localization data
-      } catch (error) {
-        vscode.window.showErrorMessage(`Error: ${error}`);
-      }
+      await fetchLocalizations();
     })
   );
 }
 
 export function deactivate() {
   console.log("ABP Localization Helper inactive");
+}
+
+async function fetchLocalizations() {
+  try {
+    const config = vscode.workspace.getConfiguration("abp-localization-helper");
+    const apiUrl = config.get<string>("apiUrl");
+    const cultureNames = config.get<string[]>("cultureNames") ?? [];
+    const defaultResourceName = config.get<string>("defaultResourceName");
+
+    if (!apiUrl) {
+      vscode.window.showErrorMessage("API URL is not configured. Please set it in the settings.");
+      return;
+    }
+
+    const localizationData = await fetchLocalizationData(apiUrl, cultureNames);
+
+    for (const cultureName of cultureNames) {
+      const resources = localizationData[cultureName].resources;
+
+      localizationsByCulture[cultureName] = Object.keys(resources).reduce(
+        (localizations: Record<string, string>, resourceKey) => {
+          const resource = resources[resourceKey];
+
+          for (const localizationKey in resource.texts) {
+            localizations[resourceKey + "::" + localizationKey] = resource.texts[localizationKey];
+
+            if (defaultResourceName === resourceKey) {
+              localizations["::" + localizationKey] = resource.texts[localizationKey];
+            }
+          }
+
+          return localizations;
+        },
+        {}
+      );
+    }
+
+    console.debug("localizationsByCulture", localizationsByCulture);
+  } catch (error) {
+    vscode.window.showErrorMessage(`${error instanceof Error ? error.message : error}`);
+  }
 }
